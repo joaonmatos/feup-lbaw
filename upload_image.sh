caaff355928cbe6c93c3f2ef46f3e720b61476c2
Goncalo101 <up201705494@fe.up.pt>
2020-03-02 14:18:08 +0000

    Updated upload_image.sh with docker user and image name

--- upload_image.sh
+++ upload_image.sh
@@ -3,8 +3,8 @@
 # Stop execution if a step fails
 set -e
 
-DOCKER_USERNAME=YOUR_DOCKER_ACCOUNT # Replace by your docker hub username
-IMAGE_NAME=lbaw20GG-piu
+DOCKER_USERNAME=lbaw2032 # Replace by your docker hub username
+IMAGE_NAME=lbaw2032-piu
 
 docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
 docker push $DOCKER_USERNAME/$IMAGE_NAME
