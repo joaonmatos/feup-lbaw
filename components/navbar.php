c0f86342a41f8b5165dd61940728006a5a195107
João Nuno Matos <joao.nuno.matos@fe.up.pt>
2020-03-05 18:28:05 +0000

    Switch navbar to php component

---
+++ components/navbar.php
@@ -0,0 +1,79 @@
+<?php
+function Navbar($user)
+{
+?>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        <a href="#" class="navbar-brand">news.ly</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-content">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbar-content">
+            <div class="navbar-nav mr-auto">
+                <?php NavMenu($user); ?>
+            </div>
+            <?php
+            SearchForm();
+            if ($user != null) {
+                UserMenu($user);
+            } else {
+                SessionButtons();
+            }
+            ?>
+        </div>
+    </nav>
+<?php
+}
+function SessionButtons()
+{ ?>
+<button class="btn btn-outline-light mx-md-2">Sign Up</button>
+<button class="btn btn-light mx-md-2 ">Log In</button>
+<?php }
+function NavMenu($user)
+{ ?>
+    <div class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+            #topic
+        </a>
+        <div class="dropdown-menu">
+            <span class="dropdown-item text-muted mx-2">Navigation</span>
+            <form class="dropdown-item form-inline">
+                <input class="form-control border-secondary" type="text" placeholder="Go to topic">
+                <button class="btn btn-outline-secondary my-2 my-sm-0" type="submit"><i class="fas fa-angle-right"></i></button>
+            </form>
+            <a class="dropdown-item" href="#"><i class="fas fa-home mx-2 text-muted"></i>Home</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-globe mx-2 text-muted"></i>Global Feed</a>
+            <div class="dropdown-divider"></div>
+            <span class="dropdown-item text-muted mx-2"><?= $user === null ? "Recommended" : "My" ?> Topics</span>
+            <a class="dropdown-item" href="#"><i class="fas fa-hashtag mx-2 text-muted"></i>topic1</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-hashtag mx-2 text-muted"></i>topic2</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-hashtag mx-2 text-muted"></i>topic3</a>
+            <a href="#" class="dropdown-item text-muted"><i class="fas fa-ellipsis-h mx-2"></i>See All</a>
+        </div>
+    </div>
+<?php
+}
+function UserMenu($name)
+{ ?>
+    <div class="nav-item dropdown active">
+        <a class="nav-link dropdown-toggle text-light px-0 px-md-3" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
+            <?= $name ?>
+        </a>
+        <div class="dropdown-menu dropdown-menu-right">
+            <a class="dropdown-item" href="#"><i class="fas fa-user mx-2"></i>Profile</a>
+            <a class="dropdown-item" href="#"><i class="fas fa-cog mx-2"></i>Settings</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item text-danger" href="#"><i class="fas fa-sign-out-alt mx-2"></i>Log Out</a>
+        </div>
+    </div>
+<?php
+}
+function SearchForm()
+{ ?>
+    <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light my-2 my-sm-0" type="submit"><i class="fas fa-search"></i></button>
+    </form>
+<?php
+}
+?>
