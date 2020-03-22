8ad44606aeaa24f543edb770b33055e51d2d2363
joanaferreira0011 <joanaferreira0011@gmail.com>
2020-03-22 19:56:26 +0000

    added ON UPDATE and ON DELETE and only_one_value constraints

--- src/database/database.sql
+++ src/database/database.sql
@@ -35,7 +35,7 @@ CREATE TABLE topic(
 CREATE TABLE story(
     id SERIAL PRIMARY KEY,
     title TEXT NOT NULL,
-    author_id INTEGER REFERENCES member(id),
+    author_id INTEGER REFERENCES member(id) ON UPDATE CASCADE ON DELETE SET NULL,
     published_date TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
     reality_check NUMERIC NOT NULL CONSTRAINT reality_check_ck CHECK ((reality_check >= 0) AND (reality_check <= 1))
 );
@@ -45,57 +45,67 @@ CREATE TABLE comment(
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     author_id INTEGER REFERENCES member(id),
-    published_date TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
-    reality_check NUMERIC NOT NULL CONSTRAINT reality_check_ck CHECK ((reality_check >= 0) AND (reality_check <= 1))
+    published_date TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL, 
+    reality_check NUMERIC NOT NULL CONSTRAINT reality_check_ck CHECK ((reality_check >= 0) AND (reality_check <= 1)),
+    comment_id INTEGER REFERENCES comment(id) ON UPDATE CASCADE ON DELETE SET NULL,
+    story_id INTEGER REFERENCES story(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    constraint only_one_value 
+        check (        (story_id is null or comment_id is null) 
+               and not (story_id is null and comment_id is null) )
 );
 
 -- R02
 CREATE TABLE friend(
-    user_id INTEGER REFERENCES member(id),
-    friend_id INTEGER REFERENCES member(id),
+    user_id INTEGER REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    friend_id INTEGER REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (user_id, friend_id)
 );
 
 -- R03
 CREATE TABLE favourites(
-    user_id INTEGER REFERENCES member(id),
-    topic_id INTEGER REFERENCES topic(id),
+    user_id INTEGER REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    topic_id INTEGER REFERENCES topic(id) ON UPDATE CASCADE ON DELETE CASCADE,
     PRIMARY KEY (user_id, topic_id)
 );
 
 -- R04
 CREATE TABLE expert(
-    user_id INTEGER REFERENCES member(id) PRIMARY KEY,
-    topic_id INTEGER REFERENCES topic(id)
+    user_id INTEGER REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE  PRIMARY KEY,
+    topic_id INTEGER REFERENCES topic(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 -- R07
 CREATE TABLE belongs_to(
-    story_id INTEGER REFERENCES story(id) PRIMARY KEY,
-    topic_id INTEGER REFERENCES topic(id)
+    story_id INTEGER REFERENCES story(id) ON UPDATE CASCADE ON DELETE CASCADE PRIMARY KEY,
+    topic_id INTEGER REFERENCES topic(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
 -- R08
 CREATE TABLE rates_story(
-    user_id INTEGER REFERENCES member(id),
-    story_id INTEGER REFERENCES story(id),
+    user_id INTEGER REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    story_id INTEGER REFERENCES story(id) ON UPDATE CASCADE ON DELETE CASCADE,
     rating BOOLEAN NOT NULL,
     PRIMARY KEY(user_id, story_id)
 );
 
 -- R09
 CREATE TABLE rates_comment(
-    user_id INTEGER REFERENCES member(id),
-    comment_id INTEGER REFERENCES comment(id),
+    user_id INTEGER REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    comment_id INTEGER REFERENCES comment(id) ON UPDATE CASCADE ON DELETE CASCADE,
     rating BOOLEAN NOT NULL,
     PRIMARY KEY(user_id, comment_id)
 );
 
 -- R11
 CREATE TABLE report(
-    id INTEGER REFERENCES member(id),
+    id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     published_date TIMESTAMP WITH TIME zone DEFAULT now() NOT NULL,
-    user_id INTEGER REFERENCES member(id)
+    user_id INTEGER REFERENCES member(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    comment_id INTEGER REFERENCES comment(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    story_id INTEGER REFERENCES story(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    constraint only_one_value 
+        check (        (story_id is null or comment_id is null) 
+               and not (story_id is null and comment_id is null) )
 );
