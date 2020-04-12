be675648b64f23d28333105dce77d80f01ee56b8
João Nuno Matos <joao.nuno.matos@fe.up.pt>
2020-04-12 17:27:45 +0100

    Add our skeleton

---
+++ upload_image.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Stop execution if a step fails
+set -e
+
+DOCKER_USERNAME=lbaw2032
+IMAGE_NAME=lbaw2032
+
+# Ensure that dependencies are available
+composer install
+php artisan clear-compiled
+php artisan optimize
+
+docker build -t $DOCKER_USERNAME/$IMAGE_NAME .
+docker push $DOCKER_USERNAME/$IMAGE_NAME
