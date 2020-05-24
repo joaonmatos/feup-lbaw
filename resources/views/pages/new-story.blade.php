e230e6555a3ed128c1f96c69e10defc4b43d3425
Goncalo101 <up201705494@fe.up.pt>
2020-05-25 00:07:48 +0100

    Fixed wrong tag

--- resources/views/pages/new-story.blade.php
+++ resources/views/pages/new-story.blade.php
@@ -16,21 +16,21 @@
         @endif
         <div class="my-2">
             <label for="title" class="h3">Title</label>
-            </br>
+            <br>
             <input type="text" name="title" id="title" value="{{ old('title') }}">
         </div>
         <div class="my-2">
             <label for="link" class="h3">Link</label>
-            </br>
+            <br>
             <input type="url" name="link" id="link" value="{{ old('link') }}">
         </div>
         <div class="my-2">
             <span class="h3">Topics</span>
-            </br>
+            <br>
             <input type="text" name="topic1" id="topic1" class="my-1" value="{{ old('topic1') }}">
-            </br>
+            <br>
             <input type="text" name="topic2" id="topic2" class="my-1" value="{{ old('topic2') }}">
-            </br>
+            <br>
             <input type="text" name="topic3" id="topic3" class="my-1" value="{{ old('topic3') }}">
         </div>
         <button type="submit" class="btn btn-primary my-2">Post Story</button>
